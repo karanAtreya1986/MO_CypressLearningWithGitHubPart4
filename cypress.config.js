d437080cf0fb32f9f94c5ef6a69c25c395e37e46
karanAtreya1986 <karan1988@gmail.com>
2025-07-14 17:59:00 +0530

    second commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,26 +2,24 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-    autoOpen:true,
-    overwrite:false,
-    code:false
-  },
-  e2e: {
+   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // require('cypress-mochawesome-reporter/plugin')(on);
     },
-    watchForFileChanges:true,
-    retries:{
-      runMode:2,
-      openMode:3
-    }
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite:false,
+    html: false,
+    json: true,
+  },
+ 
+    // watchForFileChanges:true,
+    // retries:{
+    //   runMode:2,
+    //   openMode:3
+    // }
+  // },
 });
