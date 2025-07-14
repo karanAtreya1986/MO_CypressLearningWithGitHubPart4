3071dbb7422c9424c407de833563e35afd9c259e
karanAtreya1986 <karan1988@gmail.com>
2025-07-14 18:28:36 +0530

    fourth commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,21 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
-   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // require('cypress-mochawesome-reporter/plugin')(on);
-    },
-  },
-  reporter: 'mochawesome',
+  
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     reportDir: 'cypress/results',
     overwrite:false,
-    html: false,
+    html: true,
     json: true,
   },
+   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    }
+  }
  
     // watchForFileChanges:true,
     // retries:{
